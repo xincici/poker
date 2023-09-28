d60eadc27d240f91b3e70a118f9f13fa698a7a78
xincici <llxy8687@foxmail.com>
2023-09-28 18:07:13 +0800

    feat: add card animation but not perfect

--- src/components/CardItem.vue
+++ src/components/CardItem.vue
@@ -2,26 +2,27 @@
   <span
     class="card"
     :class="{
-      'card-red': isRed,
+      'card-red': isRed && !b2a,
       'card-mini': mini,
       'card-hold': hold,
+      animating: a2b || b2a,
     }"
   >
-    <span class="card-num card-num-top" v-if="point">{{ point }}</span>
-    <span class="card-num card-num-bottom" v-if="point">{{ point }}</span>
-    <span class="card-back" v-if="isBack"></span>
+    <span class="card-num card-num-top" v-if="!showBack">{{ point }}</span>
+    <span class="card-num card-num-bottom" v-if="!showBack">{{ point }}</span>
+    <span class="card-back" v-if="showBack"></span>
     <span class="card-type">
-      <i i-mdi-cards-heart v-if="type === 'heart'" />
+      <i i-mdi-cards-playing v-if="showBack" class="card-type-back" />
+      <i i-mdi-cards-heart v-else-if="type === 'heart'" />
       <i i-mdi-cards-diamond v-else-if="type === 'diamond'" />
       <i i-mdi-cards-spade v-else-if="type === 'spade'" />
       <i i-mdi-cards-club v-else-if="type === 'club'" />
-      <i i-mdi-cards-playing v-else class="card-type-back" />
     </span>
   </span>
 </template>
 
 <script setup>
-import { computed } from 'vue';
+import { ref, computed, watch } from 'vue';
 
 // 牌点数对应的展示内容
 const NUMS = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
@@ -33,6 +34,24 @@ const isBack = computed(() => !props.num || !props.type);
 const isRed = computed(() => ['heart', 'diamond'].includes(props.type));
 
 const point = computed(() => props.num ? NUMS[props.num - 1] : '');
+
+const a2b = ref(false); // card to back
+const b2a = ref(false); // back to card
+
+const showBack = computed(() => {
+  return isBack.value || b2a.value;
+});
+
+const duration = 125;
+
+watch(isBack, val => {
+  if (props.mini) return;
+  if (val) a2b.value = true;
+  else b2a.value = true;
+  setTimeout(() => {
+    a2b.value = b2a.value = false;
+  }, duration);
+});
 
 </script>
 
@@ -69,6 +88,11 @@ const point = computed(() => props.num ? NUMS[props.num - 1] : '');
   color: #111;
   cursor: pointer;
   overflow: hidden;
+  transition: transform .125s ease-out 0s;
+  transform: rotateY(0deg);
+  &.animating {
+    transform: rotateY(90deg);
+  }
   &-red {
     color: #e00;
   }
